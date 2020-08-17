633a141f2df89823937367b08c74a5591720c35a
teacher <net94@naver.com>
2020-08-17 12:53:12 +0900

    한줄 추가

--- src/JdbcUtil.java
+++ src/JdbcUtil.java
@@ -21,6 +21,7 @@ public class JdbcUtil {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("aaaaaaaaaaaa");
         }
         return conn;
     }
